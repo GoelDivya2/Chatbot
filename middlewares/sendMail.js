22986c5cbdde8fd873adaf74643cdecffc9d7356
Divya Goel <goeldivya295@gmail.com>
2026-06-10 23:50:30 +0530

    update

--- middlewares/sendMail.js
+++ middlewares/sendMail.js
@@ -4,16 +4,16 @@ const sendMail = async (email, subject, otp) => {
   try {
     console.log("📩 Sending email to:", email);
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true, // SSL
-      auth: {
-        user: process.env.Gmail,
-        pass: process.env.Password,
-      },
-    });
-
+   const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.Gmail,
+    pass: process.env.Password,
+  },
+  family: 4, // 🔥 FORCE IPv4
+});
     await transporter.verify();
     console.log("✅ SMTP connection verified");
 
