27c573c4573d398bbc52432ab4511ce458d0303a
Divya Goel <goeldivya295@gmail.com>
2026-06-10 23:30:36 +0530

    update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./database/db.js";
 import cors from "cors";
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 
 dotenv.config();
 
